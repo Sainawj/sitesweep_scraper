ac89d6bd052130b831b8e107cd15f74c30834855
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-09-16 13:32:04 +0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,12 @@
 CREATE TABLE scraping_history (
     id INT AUTO_INCREMENT PRIMARY KEY,
     url VARCHAR(255) NOT NULL,
-    data TEXT NOT NULL,
+    data TEXT,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(255),
+    description TEXT,
+    emails TEXT,
+    phones TEXT,
+    addresses TEXT;
     status VARCHAR(50) DEFAULT 'Completed'
 );
