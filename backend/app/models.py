04e5c413161fd5e43cc9b672a5d2fc2e3b85bfd7
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-09-17 16:17:29 +0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -37,4 +37,4 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.username}>'
