e8b651cec405d749aebd259225ab961de7c26b07
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-09-17 13:36:28 +0300

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from dotenv import load_dotenv
 
 # Load environment variables from the .env file
@@ -9,6 +10,7 @@ load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()  # Initialize LoginManager
 
 def create_app():
     app = Flask(__name__, static_folder='../../frontend', template_folder='../../frontend')
@@ -20,6 +22,10 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)  # Initialize LoginManager with app
+
+    # Tell Flask-Login what view to redirect unauthorized users to
+    login_manager.login_view = 'main.login'
 
     # Import and register the Blueprint only after the app is created
     from .routes import main as main_blueprint
@@ -28,5 +34,10 @@ def create_app():
     # Import models here to avoid circular import
     with app.app_context():
         from .models import User
+
+    # Set up the user_loader callback to reload user object from user ID
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
