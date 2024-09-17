f1b65e08513073dcb843ab27fab22d115df9ba7f
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-09-17 13:34:08 +0300

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,30 +1,32 @@
-from flask_login import LoginManager
-from .models import User  # Assuming User is in models.py
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
 
-login_manager = LoginManager()
+# Load environment variables from the .env file
+load_dotenv()
 
-@login_manager.user_loader
-def load_user(user_id):
-    # Assuming `User` has a method to get a user by id
-    return User.query.get(int(user_id))
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__, static_folder='../../frontend', template_folder='../../frontend')
-
+    
     # Load config from environment variables
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '4871b4b9cd4a257721a09f44ecca7866c60479a5bd184ef699249b2e56de32b3')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'defaultsecretkey')
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Initialize the login manager
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'  # Adjust this to your actual login route
-
-    # Import and register the Blueprint
+    # Import and register the Blueprint only after the app is created
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Import models here to avoid circular import
+    with app.app_context():
+        from .models import User
 
     return app
