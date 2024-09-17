2bdfecc08aa5b9dc9ade74b79597fb089bc68e58
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-09-17 14:31:01 +0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from . import db
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class ScrapingHistory(db.Model):
     __tablename__ = 'scraping_history'
@@ -18,6 +20,21 @@ class ScrapingHistory(db.Model):
 
     def __repr__(self):
         return f'<ScrapingHistory {self.url}>'
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.TIMESTAMP, nullable=True, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
